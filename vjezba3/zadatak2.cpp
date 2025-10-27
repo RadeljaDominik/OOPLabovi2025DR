776acf3af80cff186b9e5405ab82c15ef4940f2c
RadeljaDominik <jkjbknesw@gnail.com>
2025-10-27 18:56:44 +0100

    2/4

--- vjezba3/zadatak2.cpp
+++ vjezba3/zadatak2.cpp
@@ -3,19 +3,23 @@
 
 using namespace std;
 
-inline bool ascending (int a, int b){
+template<typename type>
+inline bool ascending (type a, type b){
     return a > b;
 }
 
-inline bool descending (int a, int b){
+template<typename type>
+inline bool descending (type a, type b){
     return a < b;
 }
 
-void sortt(int niz[], size_t n, bool (*cmp)(int, int)){
+
+template<typename type>
+void sortt(type niz[], size_t n, bool (*cmp)(int, int)){
     for (size_t i=0;i<n-1;i++){
         for(size_t j=0;j<n-i-1;j++){
             if(cmp(niz[j+1],niz[j])){
-                int temp=niz[j];
+                type temp=niz[j];
                 niz[j]=niz[j+1];
                 niz[j+1]=temp;
             }
@@ -32,6 +36,6 @@ int main(){
     size_t n=sizeof(niz)/sizeof(int);
     sortt(niz,n,ascending);
     sortt(niz,n,descending);
-
     return 0;
 }
+
