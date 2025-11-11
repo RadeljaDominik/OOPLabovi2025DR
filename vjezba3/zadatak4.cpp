864a7102be2c15fac9912346681a3f2428cd0cdb
RadeljaDominik <jkjbknesw@gmail.com>
2025-11-11 10:03:12 +0100

    vj 3 vj 5

--- vjezba3/zadatak4.cpp
+++ vjezba3/zadatak4.cpp
@@ -2,7 +2,57 @@
 
 using namespace std;
 
+typedef struct {
+    string ime;
+    string jmbag;
+    int studyY;
+    int ECTS;
+    double prosjek;
+} Student;
+
+void filtriraj(Student st[], size_t s, void (*akcija)(Student&), bool (*filter)(Student&)){
+    for (size_t i=0; i<s; i++){
+        if(filter(st[i])){
+            akcija(st[i]);
+        }
+    }
+}
+
+void ispis(Student& st){
+    cout << "Ime: "<<st.ime<<endl;
+    cout << "JMBAG: "<<st.jmbag<<endl;
+    cout << "Godina: "<<st.studyY<<endl;
+    cout << "ECTS: "<<st.ECTS<<endl;
+    cout << "Prosjek ocjena: "<<st.prosjek<<endl;
+}
+
+void godinaUp(Student& st){
+    st.studyY++;
+}
+
+
 int main(){
+    Student studenti[3]={
+        {"Ivo Ivic","11111111",1,0,0.0},
+        {"Mate Matic","22222222",2,50,3.9},
+        {"Ante Antic","33333333",3,70,2.8}
+    };
+
+    auto jedanIs = [](Student& s){
+        return s.studyY == 1 && s.ECTS > 0;
+    };
+    filtriraj(studenti, 3, ispis, jedanIs);
+
+    auto prosjek = [](Student& s){
+        return s.prosjek > 3.5;
+    };
+    filtriraj(studenti, 3, ispis, prosjek);
+
+    auto ects45 = [](Student& s){
+        return s.ECTS > 45;
+    };
+    filtriraj(studenti, 3, godinaUp, ects45);
+
 
 
     return 0;
--- vjezba5/treseta.cpp
+++ vjezba5/treseta.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <random>
 
 using namespace std;
 
@@ -6,36 +10,141 @@ class karta {
     public:
         int broj;
         char zog;
+
+        karta(char z, int b){
+            broj = b;
+            zog = z;
+        }
+        void ispis(){
+            cout<<broj<<" "<<zog;
+        }
 };
 
 class mac {
+    vector <karta> mkarti;
     public:
-        karta[40] mKarti;
-
+        mac(){
+            vector <char> mzog = {'K','S','B','D'};
+            for(const auto &z : mzog){
+                for(int i=1;i<11;i++){
+                    mkarti.push_back(karta(z,i));
+                }
+            }
+        }
+        void mijesanje(){
+            auto rng = default_random_engine{};
+            shuffle(mkarti.begin(),mkarti.end(),rng);
+        }
+        vector<karta> dijeli(int n){
+            vector<karta> ruka;
+            for(int i=0; i<n && !mkarti.empty(); i++){
+                ruka.push_back(mkarti.back());
+                mkarti.pop_back();
+            }
+            return ruka;
+        }
 };
 
 class igrac {
+    string ime;
+    int bodovi=0;
+    vector <karta> ruka;
     public:
-        string ime;
-        int bodovi;
-        karta[10] ruka;
+        igrac(string i){
+            ime = i;
+        }
+        void nRuka(vector<karta> k){
+            ruka = k;
+        }
+        void outRuka(){
+            cout<<endl<<ime<<endl<<"Ruka: ";
+            for(auto &k : ruka){
+                cout<<" ";
+                k.ispis();
+                cout<<" ";
+            }
+        }
+        void akuza(){
+            vector<char> zog = {'K','S','B','D'};
+            for(auto &z : zog){
+                bool A1=false;
+                bool A2=false;
+                bool A3=false;
+                for(auto &k : ruka){
+                    if(k.zog == z){
+                        if(k.broj == 1){
+                            A1 = true;
+                        }
+                        else if(k.broj == 2){
+                            A2 = true;
+                        }
+                        else if(k.broj == 3){
+                            A3 = true;
+                        }
+                    }
+                }
+                if(A1 && A2 && A3){
+                    bodovi +=3;
+                }
+            }
+
+            for(int i=1; i<11; i++){
+                int brojac=0;
+                for(auto &k : ruka){
+                    if(k.broj==i){
+                        brojac++;
+                    }
+                }
+                if(brojac == 3){
+                    bodovi+=3;
+                }
+                else if(brojac == 4){
+                    bodovi +=4;
+                }
+            }
+        }
+        int outBodovi(){
+            return bodovi;
+        }
+
+        string outIme(){
+            return ime;
+        }
 };
 
 int main(){
     int brojigraca;
     cout<<"Unesi broj igraca (2 ili 4)"<<endl;
     cin>>brojigraca;
-    switch(brojigraca){
-    case 2:
-
-        break;
-    case 4:
-
-        break;
-    default:
+    if(brojigraca!=2 && brojigraca != 4){
         cout<<"Nemoguc broj igraca!";
-        break;
+        return 1;
     }
 
+    vector<igrac> igraci;
+    for(int i=0; i<brojigraca; i++){
+        string ime;
+        cout<<"Unesi ime igraca"<<endl;
+        cin>>ime;
+        cout<<endl;
+        igraci.push_back(ime);
+    }
+
+    mac nmac;
+    nmac.mijesanje();
+
+    for(auto &igrac : igraci){
+        igrac.nRuka(nmac.dijeli(10));
+        igrac.akuza();
+    }
+
+    cout<<"Rezultati: "<<endl;
+    for(auto &igrac : igraci){
+        igrac.outIme();
+        cout<<endl;
+        igrac.outRuka();
+        cout<<endl;
+        cout<<"Bodovi: "<<igrac.outBodovi()<<endl;
+    }
     return 0;
 }
